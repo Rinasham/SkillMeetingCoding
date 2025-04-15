7db672b370e0e0e8a94285cf7fbc6661ecf32a41
Rinasham <rinashamau@gmail.com>
2025-04-16 02:25:42 +1000

    クッキーボタンを最初から表示

--- cookieCounter/click-counter/script.js
+++ cookieCounter/click-counter/script.js
@@ -1,42 +1,27 @@
 // HTMLからボタンの要素を取得
-let button = document.getElementById("counter-button");
-let counterNum = 0;
-let cookieButton = false;
+const counterButton = document.getElementById("counter-button");
+const cookieButton = document.getElementById("cookie-button");
+let counter = 0;
+let cookie = 0;
+const countText = document.getElementById("counts");
+const cookieText = document.getElementById("cookies");
 
-// ボタンをクリックするイベント時に発火
-button.addEventListener("click", function () {
-  // 押されるたびにカウンターに表示される数字を１ずつ増やす
-  counterNum += 1;
-  let counter = document.getElementById("counter");
-  counter.textContent = `${counterNum} カウント`;
+// カウンターボタンをクリックするイベント
+counterButton.addEventListener("click", function () {
+  // カウンターの数字を１増やして画面を書き換える
+  counter += 1;
+  countText.textContent = `${counter} カウント`;
+});
 
-  // カウンターの数字が５に増えた時の処理
-  if (counterNum == 5) {
-    // まだ「クッキーを買う」ボタンがない場合は作成して画面上に表示
-    const newButton = document.createElement("button"); // ボタン要素の作成
-    if (cookieButton == false) {
-      newButton.textContent = "クッキーを買う";
-      document.body.appendChild(newButton); // HTML上に追加
-      // booleanをtrueにして２回目以降は「クッキーを買う」ボタンが作られないようにする
-      cookieButton = !cookieButton;
-    }
-    // 「クッキーを買う」ボタンが既に存在している場合
-    let cookies = 0;
-    const cookieNum = document.createElement("p"); // pタグの要素を作成
-    document.body.appendChild(cookieNum); // HTMLに要素を追加
-
-    // 「クッキーを買う」ボタンを押した時に発火
-    newButton.addEventListener("click", function () {
-      // カウンターの数字が５に満たない場合は何もせずリターン
-      if (counterNum < 5) {
-        return;
-      }
-      // カウンターの数字を -5 する
-      counterNum -= 5;
-      counter.textContent = `${counterNum} カウント`;
-      cookies += 1;
-      // HTML上のクッキーの枚数を書き換え
-      cookieNum.textContent = `${cookies} クッキー`;
-    });
+// 「クッキーを買う」ボタンを押すイベント
+cookieButton.addEventListener("click", function () {
+  // カウンターの数字が５に満たない場合は何もせずリターン
+  if (counter < 5) {
+    return;
   }
+  // カウンターの数字を５減らし、クッキーの数字を１増やして画面を書き換える
+  counter -= 5;
+  countText.textContent = `${counter} カウント`;
+  cookie += 1;
+  cookieText.textContent = `${cookie} クッキー`;
 });
